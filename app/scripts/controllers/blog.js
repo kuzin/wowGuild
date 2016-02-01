68735ddb004735af14cf43905da1af32396354bf
Kuzin <mike@kuzn.me>
2016-01-31 20:00:58 -0500

    test

--- app/scripts/controllers/blog.js
+++ app/scripts/controllers/blog.js
@@ -25,4 +25,23 @@ angular
      });
 
 
+  })
+  .controller('PostCtrl', function ($scope, $http, Backand) {
+
+    $scope.title = '';
+    $scope.date = '';
+    $scope.content = '';
+
+    $scope.postBlog = function () {
+      var data = $.param({
+          json: JSON.stringify({
+              title: $scope.title,
+              date: new Date(),
+              content: $scope.content
+          })
+      });
+      $http.post(Backand.getApiUrl() + '/1/objects/posts', data).success(function(data, status) {
+        console.log(data);
+      });
+    };
   });
