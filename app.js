d257139a11d8863d6f7cad21f2c57e40e69db59b
Kuzin <mike@kuzn.me>
2016-01-31 19:20:16 -0500

    Another shot.

--- app.js
+++ app.js
@@ -1,12 +1,8 @@
+var gzippo = require('gzippo');
+var express = require('express');
+var morgan = require('morgan');
+var app = express();
 
-// Modules
-var gzippo    = require('gzippo');
-var express   = require('express');
-var morgan    = require('morgan');
-var app       = express();
-
-// Config
-var port = process.env.PORT || 5000;
 app.use(morgan('dev'));
 app.use(gzippo.staticGzip("" + __dirname + "/dist"));
-app.listen(port);
+app.listen(process.env.PORT || 5000);
